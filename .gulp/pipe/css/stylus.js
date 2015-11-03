23230ff9959222f93f8eb9f96e1503852f38524c
Jeremie Patonnier <jpatonnier@clever-age.com>
2015-11-03 11:30:04 +0100

    Add stylus pipelining

---
+++ .gulp/pipe/css/stylus.js
@@ -0,0 +1,33 @@
+// Définition du LazyPipe pour utiliser compass
+'use strict';
+
+// MODULES
+// ----------------------------------------------------------------------------
+var lazypipe = require('lazypipe');
+var stylus   = require('gulp-stylus');
+var stylint  = require('gulp-stylint');
+var ENV      = require('../../tools/env');
+
+// STYLUS CONFIGURATION
+// ----------------------------------------------------------------------------
+var CONF = {
+  compress:  ENV.all.optimize || false,
+  linenos : !ENV.all.optimize,
+  'include-css': true
+};
+
+// LINTER CONFIGURATION
+// ----------------------------------------------------------------------------
+var LINT = {
+  config: './.stylintrc'
+};
+
+module.exports = function () {
+  var lazystream = lazypipe()
+    .pipe(stylint, LINT)
+    .pipe(stylint.reporter)
+    .pipe(stylint.reporter, 'fail')
+    .pipe(stylus, CONF);
+
+  return lazystream();
+};
