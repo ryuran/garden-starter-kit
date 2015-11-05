270ffc608883c8cb18c63c1f1b488c37b2b5ba95
Jeremie Patonnier <jpatonnier@clever-age.com>
2015-11-05 20:46:24 +0100

    refine data used by Twig templates

--- .gsk/pipe/html/twig.js
+++ .gsk/pipe/html/twig.js
@@ -2,13 +2,28 @@
 'use strict';
 
 var path     = require('path');
+var _        = require('underscore');
 var lazypipe = require('lazypipe');
 var twig     = require('gulp-twig');
 var data     = require('gulp-data');
 var ENV      = require('../../tools/env').html;
 
+var genericDataFile  = path.resolve(path.join(ENV['data-dir'], 'data.json'));
+
 function processData(file) {
-  return require(path.resolve(path.join(ENV['data-dir'], path.basename(file.path, '.twig') + '.json')));
+  var specificDataFile = path.resolve(path.join(ENV['data-dir'], path.basename(file.path, '.twig') + '.json'));
+  var gData = {};
+  var sData = {};
+
+  try {
+    gData = require(genericDataFile);
+  } catch (e) {}
+
+  try {
+    sData = require(specificDataFile);
+  } catch (e) {}
+
+  return _.extend({}, gData, sData);
 }
 
 module.exports = function () {
