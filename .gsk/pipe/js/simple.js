ce37b4c6fe36525ccdbd2fc6bf9c7d75091a8383
Jeremie Patonnier <jpatonnier@clever-age.com>
2015-12-21 14:36:11 +0100

    js pipeline are all different

--- .gsk/pipe/js/simple.js
+++ .gsk/pipe/js/simple.js
@@ -1,22 +1,38 @@
-// Définition du LazyPipe pour utiliser compass
+// Définition de gulp js version "simple"
 'use strict';
 
 // MODULES
 // ----------------------------------------------------------------------------
-var lazypipe  = require('lazypipe');
-var gulpif    = require('gulp-if');
+var path      = require('path');
+var gulp      = require('gulp');
+var gif       = require('gulp-if');
+var plumber   = require('gulp-plumber');
 var concat    = require('gulp-concat');
 var sourcemap = require('gulp-sourcemaps');
 var uglify    = require('gulp-uglify');
+var bs        = require('browser-sync');
+var err       = require('../../tools/errcb');
 var ENV       = require('../../tools/env');
 
+var SRC  = [
+  path.join(ENV.js['src-dir'], 'lib', '**', '*'),
+  path.join(ENV.js['src-dir'], '**', '*')
+];
+var DEST = ENV.js['dest-dir'];
 
-module.exports = function () {
-  return lazypipe()
-    .pipe(sourcemap.init)
-    .pipe(concat, 'scripts.js')
-    .pipe(function () {
-      return gulpif(ENV.all.optimize, uglify());
-    })
-    .pipe(sourcemap.write, '.')();
-};
+
+// TASK DEFINITION
+// ----------------------------------------------------------------------------
+// $ gulp js
+// ----------------------------------------------------------------------------
+// Gère toutes les actions d'assemblage JavaScript
+gulp.task('js', ['test:js'], function () {
+  return gulp.src(SRC)
+    .pipe(plumber({ errorHandler: err }))
+    .pipe(sourcemap.init())
+    .pipe(concat(ENV.js.filename))
+    .pipe(gif(ENV.all.optimize, uglify()))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest(DEST))
+    .pipe(bs.stream());
+});
