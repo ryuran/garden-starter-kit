98ec765825d5e996447f2a92c023c0ff34a0083f
Stephane Tessier <stessier@clever-age.com>
2016-05-24 18:12:15 +0200

    More flexible postcss configuration

--- .gsk/tasks/css.js
+++ .gsk/tasks/css.js
@@ -6,10 +6,10 @@ var path         = require('path');
 var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
 var postcss      = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
 var bs           = require('browser-sync');
 var err          = require('../tools/errcb');
 var ENV          = require('../tools/env').css;
+var _            = require('underscore');
 
 // On ne va compiler que les fichiers dont le nom ne commence pas par un _
 var SRC  = [
@@ -25,25 +25,20 @@ var DEST = ENV['dest-dir'];
 var pipeline = require('../pipe/css/' + ENV.engine + '.js');
 
 
-// AUTOPREFIXER CONFIGURATION
-// ----------------------------------------------------------------------------
-var APX_CONF = {
-  browsers: ENV.autoprefixer || ['> 4%', 'ie >= 8']
-};
-
-
 // TASK DEFINITION
 // ----------------------------------------------------------------------------
 // $ gulp css
 // ----------------------------------------------------------------------------
 // Gère la compilation des fichiers CSS
 gulp.task('css', ['test:css'], function () {
+  var processors = _.map(ENV.postcss, function(conf, processorName) {
+      var processor = require(processorName);
+      return processor(conf);
+    });
   return gulp.src(SRC)
     .pipe(plumber({ errorHandler: err }))
     .pipe(pipeline())
-    .pipe(postcss([
-      autoprefixer(APX_CONF)
-    ]))
+    .pipe(postcss(processors))
     .pipe(gulp.dest(DEST))
     .pipe(bs.stream());
 });
