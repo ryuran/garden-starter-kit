eb7460b7f14447abf8e818385b07264948a2f88c
Yvain Liechti <yliechti@clever-age.com>
2016-07-18 17:56:41 +0200

    remove compass option in eyeglass pipe

--- .gsk/pipe/css/eyeglass.js
+++ .gsk/pipe/css/eyeglass.js
@@ -11,13 +11,8 @@ var ENV = require('../../tools/env');
 // SASS CONFIGURATION
 // ----------------------------------------------------------------------------
 var SASS = {
-  bundle_exec: true,
-  config_file: './config.rb',
-  environment: ENV.all.optimize ? 'production' : 'development',
   sass       : ENV.css['src-dir'],
   css        : ENV.css['dest-dir'],
-  relative   : false,
-
   eyeglass: {
     // put eyeglass options you need here.
   }
