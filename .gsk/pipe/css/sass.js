d755dd4b9474c019468bd30238b270243d8ef0e0
Stephane Tessier <stessier@clever-age.com>
2016-07-28 15:24:21 +0200

    Adding force option on gulp css with compass

--- .gsk/pipe/css/sass.js
+++ .gsk/pipe/css/sass.js
@@ -15,7 +15,11 @@ var SASS = {
   environment: ENV.all.optimize ? 'production' : 'development',
   sass       : ENV.css['src-dir'],
   css        : ENV.css['dest-dir'],
-  relative   : false
+  relative   : false,
+  // Force only if task is called via `gulp css`.
+  // That fixes the following bug: `--optimized` option was ignored if built
+  // files were already cached.
+  force      : require('yargs').argv._[0] === 'css'
 };
 
 module.exports = function () {
