2eedac4f1f69accd6ce3371127820798a4f1847c
Yvain Liechti <yliechti@clever-age.com>
2016-12-22 15:48:42 +0100

    Fix #73 remove html beautify

--- .gsk/tasks/html.js
+++ .gsk/tasks/html.js
@@ -7,7 +7,6 @@ var path     = require('path');
 var gulp     = require('gulp');
 var gutil    = require('gulp-util');
 var plumber  = require('gulp-plumber');
-var prettify = require('gulp-prettify');
 var bs       = require('browser-sync');
 var err      = require('../tools/errcb');
 var ENV      = require('../tools/env').html;
@@ -34,31 +33,6 @@ var SRC  = [
 var DEST = ENV['dest-dir'];
 
 
-// PRETTIFY CONFIGURATION
-// ----------------------------------------------------------------------------
-var PRT_CONF;
-
-try {
-  // gulp-prettify est trop con pour gérer lui même les fichiers .jsbeautifyrc
-  PRT_CONF = JSON.parse(fs.readFileSync('./.jsbeautifyrc'));
-} catch (e) {
-  gutil.log(gutil.colors.yellow('WARN:'), '[.jsbeautifyrc]', e.message);
-
-  PRT_CONF = {
-    brace_style          : 'collapse',
-    end_with_newline     : true,
-    indent_size          : 2,
-    indent_char          : ' ',
-    indent_inner_html    : false,
-    indent_scripts       : 'normal',
-    max_preserve_newlines: 2,
-    preserve_newlines    : true,
-    unformatted          : [
-      'pre', 'code', 'a', 'sub', 'sup', 'b', 'i', 'u', 'strong', 'em'
-    ]
-  };
-}
-
 // TASK DEFINITION
 // ----------------------------------------------------------------------------
 // $ gulp html
@@ -68,7 +42,6 @@ gulp.task('html', 'Compile HTML files.', function () {
   return gulp.src(SRC)
     .pipe(plumber({ errorHandler: err }))
     .pipe(pipeline())
-    .pipe(prettify(PRT_CONF))
     .pipe(gulp.dest(DEST))
     .pipe(bs.stream());
 });
