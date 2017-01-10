6c4d2a53f571a0522855b3c53301a340be8e1706
Stephane Tessier <stessier@clever-age.com>
2017-01-10 14:37:59 +0100

    Replacing gulp inject with the filecontent helper
    https://github.com/cleverage/garden-starter-kit/issues/7

--- .gsk/tasks/svg.js
+++ .gsk/tasks/svg.js
@@ -4,33 +4,40 @@
 // ----------------------------------------------------------------------------
 var gulp     = require('gulp');
 var svgstore = require('gulp-svgstore');
-var inject   = require('gulp-inject');
 var ENV      = require('../tools/env').svg;
 var path     = require('path');
+var rename   = require('gulp-rename');
 
 var SRC  = path.join(ENV['src-dir'],  '/**', '*.svg');
-var DEST = path.join(ENV['html-dir'], '/**', '*.html');
 
 // TASK DEFINITION
 // ----------------------------------------------------------------------------
 
 // $ gulp svg:symbols
 // ----------------------------------------------------------------------------
-// Inclus des svg du dossier symbols dans le html
-gulp.task('svg:symbols', ['html'], function () {
-
-  var svgs = gulp
-      .src(SRC)
-      .pipe(svgstore({ inlineSvg: true }));
-
-  function fileContents (filePath, file) {
-    return file.contents.toString();
-  }
-
+// Regroupe tous les svg dans un même fichier afin qu'il puissent être insérés
+// dans les templates via la fonction `filecontent(path)`
+gulp.task('svg:symbols', function () {
   return gulp
-      .src(DEST)
-      .pipe(inject(svgs, { transform: fileContents }))
-      .pipe(gulp.dest(ENV['html-dir']));
+    .src(SRC)
+    .pipe(rename(function (filePath) {
+      // add subfolders (if any) into filename
+      var nameArray = filePath.dirname !== '.' ? filePath.dirname.split(filePath.sep) : [];
+      nameArray.push(filePath.basename);
+      // use prefix from config
+      if (ENV['id-prefix']) {
+        nameArray.unshift(ENV['id-prefix']);
+      }
+      filePath.basename = nameArray.join('-');
+    }))
+    .pipe(svgstore({ inlineSvg: true }))
+    // if specified in config, rename output file
+    .pipe(rename(function (filePath) {
+      if (ENV['dest-file']) {
+        filePath.basename = ENV['dest-file'];
+      }
+    }))
+    .pipe(gulp.dest(ENV['dest-dir']));
 });
 
 // $ gulp svg
