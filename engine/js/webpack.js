79de1198d6a8cdec1e8f3ac5747456d1ca546db2
Yvain Liechti <yliechti@clever-age.com>
2018-01-05 14:14:52 +0100

    webpack engine to gulp4

--- engine/js/webpack.js
+++ engine/js/webpack.js
@@ -3,16 +3,16 @@
 
 // MODULES
 // ----------------------------------------------------------------------------
-var path          = require('path');
-var gulp          = require('gulp');
+var path = require('path');
+var gulp = require('gulp');
 var webpackStream = require('webpack-stream');
-var webpack       = require('webpack');
-var plumber       = require('gulp-plumber');
-var bs            = require('browser-sync');
-var err           = require('../../tools/errcb');
-var ENV           = require('../../tools/env');
+var webpack = require('webpack');
+var plumber = require('gulp-plumber');
+var bs = require('browser-sync');
+var err = require('../../tools/errcb');
+var ENV = require('../../tools/env');
 
-var SRC  = path.join(ENV.js['src-dir'], '**', '*.js');
+var SRC = path.join(ENV.js['src-dir'], '**', '*.js');
 var DEST = ENV.js['dest-dir'];
 
 var webpackConfig = require(path.relative(__dirname, path.join(process.cwd(), ENV.js.config)));
@@ -22,15 +22,15 @@ var webpackConfig = require(path.relative(__dirname, path.join(process.cwd(), EN
 // $ gulp js
 // ----------------------------------------------------------------------------
 // Gère toutes les actions d’assemblage JavaScript
-gulp.task('js', 'Compile JS files into build folder using webpack.', ['test:js'], function () {
+gulp.task('js', function () {
   return gulp.src(SRC)
     .pipe(plumber({ errorHandler: err }))
     .pipe(webpackStream(webpackConfig(ENV), webpack))
     .pipe(gulp.dest(DEST))
     .on('end', bs.reload);
-}, {
-  options: {
-    optimize : 'Optimize for production.',
-    relax    : 'Skip tests. ☠ ☠ ☠'
-  }
 });
+gulp.task('js').description = 'Compile JS files into build folder using webpack.';
+gulp.task('js').flags = {
+  '--optimize': 'Optimize for production.',
+  '--relax': 'Skip tests. ☠ ☠ ☠'
+};
